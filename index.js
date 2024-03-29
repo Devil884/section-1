e549c49177bfce4512354cb92dac3191d795e57e
Devil884 <7376890457atul@gmail.com>
2024-03-29 15:16:59 +0530

    created js

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('Hello JavaScript');
+num = 3456;
+console.log(num);
+console.log(typeof num);
+ fruit = 'apple';
+ console.log(typeof num);
+ console.log('1'+1);
+ a=56;
+ b=2;
+ console.log(a*b)
+ console.log(a**b)
