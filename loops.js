275645f243f102a0d7392badc21dad2578da78f8
Devil884 <7376890457atul@gmail.com>
2024-03-29 15:26:57 +0530

    created loops in js

---
+++ loops.js
@@ -0,0 +1,9 @@
+for(let i=100; i<=350; i++){
+    if(i%7 === 0 && i%11 === 0){
+        console.log(i);
+    }
+}
+const fruit = ['apple','lichi','guva','graph'];
+for(let f of fruit){
+    console.log(f);
+}
