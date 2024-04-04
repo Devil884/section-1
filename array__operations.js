0acfcd7c46604863e5044ef3f9fbc777a8dc9bf8
Devil884 <7376890457atul@gmail.com>
2024-04-04 15:13:32 +0530

    last arrayOP

--- array__operations.js
+++ array__operations.js
@@ -5,4 +5,25 @@ const smartphoneList=[
     { brand :'Vivo',model :'Vivo Y7',price :'21000',colors :['white','black','yello']},
     { brand :'Apple',model :'18 pro',price :'50000',colors :['pink','black','blue']},
     { brand :'Samsung',model :'z fold',price :'162000',colors :['white']}
- ]
+ ];
+
+//  const Bphone = smartphoneList.filter((phone)=>{return phone.price<50000});
+  
+//  console.log(Bphone);
+ 
+//filter all samsung phone
+//  const Bphone = smartphoneList.filter((phone)=>{return phone.brand==='Samsung'});
+//  console.log(Bphone);
+
+ //filter all phones with blue color
+ const Bluephone = smartphoneList.filter((phone)=>{return phone.colors.includes('blue')});
+ console.log(Bluephone);
+
+ //creat an array of phone models
+ //creat an array of phone brands
+ //['s24','iphone 18','nord ce 3','note 8 pro','zfold']
+  const model = smartphoneList.map((phone)=>{ return phone.model});
+  console.log(model);
+
+  const brand = smartphoneList.map((phone)=>{ return phone.brand});
+  console.log(brand);
