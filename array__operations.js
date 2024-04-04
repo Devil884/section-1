70a184e146afbd60fb9dc921695f9c75b344c87c
Devil884 <7376890457atul@gmail.com>
2024-04-04 14:51:09 +0530

    updates

---
+++ array__operations.js
@@ -0,0 +1,8 @@
+const smartphoneList=[
+    { brand :'Samsung',model :'s24',price :'12000',colors :['white','black','blue']},
+    { brand :'Redimi',model :'note12pro',price :'18000',colors :['ger','black','blue']},
+    { brand :'poco',model :'POco X5',price :'17000',colors :['white','black','blue']},
+    { brand :'Vivo',model :'Vivo Y7',price :'21000',colors :['white','black','yello']},
+    { brand :'Apple',model :'18 pro',price :'50000',colors :['pink','black','blue']},
+    { brand :'Samsung',model :'z fold',price :'162000',colors :['white']}
+ ]
--- map-filters.js
+++ map-filters.js
@@ -15,3 +15,22 @@ const nums =[4,8,3,1,6,9];
    console.log('abc'.toUpperCase());
     const upperCaseName = names.map((n)=>{return n.toUpperCase()});
     console.log(upperCaseName);
+
+   const evenNums= nums.filter((n)=>{return n%2===0});
+    console.log(evenNums);
+
+    const prices=[530,990,2880,3200,800,120];
+    //800<prices<1500
+
+    const num =prices.filter((n)=>{return n>800 && n<1500})
+    console.log(num);
+
+    const length = names.filter((n)=>{return n.length >4});
+    console.log(length);
+     const strPrices =['₹56.90','₹780.892','₹190.8','120.9'];
+    // console.log(parseInt('₹67.82'.slice(1)));
+     const rupee = strPrices.map((n)=>{return parseInt(n.slice(1))})
+     console.log(rupee);
+
+     
+    
